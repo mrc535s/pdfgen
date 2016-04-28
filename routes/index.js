7f52d500e89944acd08c18254dea210a8d6d2d3a
Michael Carney <michaelrcarney@gmail.com>
2016-04-27 19:30:01 -0500

    Added filtes

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.send('id: ' + req.query.id);
+  var pdfGen= genPDF();
+
+  pdfGen.on('finish', function () {
+    res.download('BriefSummary.pdf');
+  });
+});
+
+router.get('/pdf/', function(req, res, next) {
+  //res.send('id: ' + req.query.id);
+  res.render('index', { title: 1});
+});
+
+function genPDF() {
+  var fs = require('fs');
+  var wkhtmltopdf = require('wkhtmltopdf');
+
+  var write = fs.createWriteStream('BriefSummary.pdf');
+
+  // URL
+  wkhtmltopdf('http://localhost:3000/pdf', { pageSize: 'letter' , orientation: 'landscape', printMediaType: true})
+    .pipe(write);
+
+  return write;
+}
+
+module.exports = router;
