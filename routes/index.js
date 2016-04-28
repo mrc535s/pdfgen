faedcee77ebdfb87b66722a82e43ae6cec6e9806
Michael Carney <michaelrcarney@gmail.com>
2016-04-27 23:17:17 -0500

    Refactored

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.send('id: ' + req.query.id);
-  var pdfGen= genPDF();
+  var data = [["title", "1"]];
+  startup(data, res);
+});
 
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  var data = mapObj(req.body);
+  console.log(data);
+  startup(data, res);
+});
+
+// router.get('/pdf/', function(req, res, next) {
+//   res.render('index', { title: 1});
+// });
+
+router.post('/pdf/', function(req, res, next) {
+  res.render('index', req.body);
+});
+
+function startup(data, res) {
+  var pdfGen= genPDF(data);
+  console.log('Data: ' + data);
   pdfGen.on('finish', function () {
-    res.download('BriefSummary.pdf');
+    res.download('BriefSummary.pdf', 'BriefSummary.pdf');
   });
-});
+}
 
-router.get('/pdf/', function(req, res, next) {
-  //res.send('id: ' + req.query.id);
-  res.render('index', { title: 1});
-});
+function mapObj(o) {
+  return Object.keys(o).map(function(k) { return [k, o[k]] });
+}
 
-function genPDF() {
+function genPDF(data) {
   var fs = require('fs');
+  //var customHeader = {'Content-Type': 'application/x-www-form-urlencoded'}
   var wkhtmltopdf = require('wkhtmltopdf');
+  var url = 'https://immense-springs-99065.herokuapp.com/';
 
   var write = fs.createWriteStream('BriefSummary.pdf');
-
   // URL
-  wkhtmltopdf('https://immense-springs-99065.herokuapp.com/pdf', { pageSize: 'letter' , orientation: 'landscape', printMediaType: true})
+  wkhtmltopdf('http://localhost:3000/pdf', { pageSize: 'letter' ,
+    orientation: 'landscape', printMediaType: true, post: data})
     .pipe(write);
 
   return write;
