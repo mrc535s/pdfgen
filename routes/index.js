fd5861590b77043933c4712e237df297dc4c4a7b
Michael Carney <michaelrcarney@gmail.com>
2016-05-24 09:58:19 -0500

    footer height

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ router.post('/', function (req, res, next) {
       html: req.body.html,
       paperSize: {
         orientation: 'landscape',
+        footerHeight: '15px'
       },
       footer: "<div style='font-size: 6.5px; margin-top: 20px;'> <span style='float: left;'> For non-spatial data, comparisons are based on the number of locations. For spatial data, comparisons are based on the number of unique location by soild type combinations. SSURGO is used to assign soil attributes to spatial trials </span> <span style='float: right'>Page {#pageNum} of {#numPages} </span>"
     }, function(err, pdf) {
