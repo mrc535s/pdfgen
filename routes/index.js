a26cfb04cfb06af840e74f6b04b0b6af93ed3bbc
Michael Carney <michaelrcarney@gmail.com>
2016-05-25 12:57:23 -0500

    cleaned up repo

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var wkhtmltopdf = require('wkhtmltopdf');
-var appRoot = require('app-root-path');
-var rk = require('randomkey');
-var base = appRoot + '/public';
-var genKey = rk(16, rk.safe); // so users won't clash image creation.
-var pdf = 'BriefSummary_' + genKey + '.pdf';
 
 
 /* POST home page. */
-router.post('/', function (req, res, next) {
-  // var options = {
-  //     pageSize: 'letter',
-  //     orientation: 'landscape',
-  //     printMediaType: true,
-  //     footerLeft: "For non-spatial data,comparisons are based on the number of locations. For spatial data,comparisons are based on the number of unique location by soild type combinations.SSURGO is used to assign soil attributes to spatial trials",
-  //     footerRight: "Page [page] of [topage]",
-  //     footerFontSize: 6,
-  //     footerLine: true,
-  //     footerFontName: "Helvetica",
-  //     footerSpacing: 1
-  //   };
-    
+router.post('/', function (req, res, next) {   
   var conversion = require("phantom-html-to-pdf")();
     conversion({ 
       html: req.body.html,
@@ -32,21 +13,11 @@ router.post('/', function (req, res, next) {
       },
       footer: "<div style='font-size: 6.5px; margin-top: 20px;'> <span style='float: left;'> For non-spatial data, comparisons are based on the number of locations. For spatial data, comparisons are based on the number of unique location by soild type combinations. SSURGO is used to assign soil attributes to spatial trials </span> <span style='float: right'>Page {#pageNum} of {#numPages} </span>"
     }, function(err, pdf) {
-      console.log(pdf.logs);
-      console.log(pdf.numberOfPages);
+      //console.log(pdf.logs);
+      //console.log(pdf.numberOfPages);
       pdf.stream.pipe(res);
     });
 });
-
-
-
-function genPDF(html, options) {
-  var write = fs.createWriteStream(pdf);
-  wkhtmltopdf(html, options)
-    .pipe(write);
-
-  return write;
-}
 
 
 module.exports = router;
